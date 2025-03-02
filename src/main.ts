2538261b45facaeac20c8ababe090b8dfe441bd7
Ivan Demenkov <ariastation@ua.fm>
2025-03-02 18:22:32 +0200

    updated views to pug

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+  app.setViewEngine('pug');
 
   await app.listen(process.env.PORT ?? 3000);
 }
